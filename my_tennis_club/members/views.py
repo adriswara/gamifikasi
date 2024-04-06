a9818279223e0b7f1bd31f9e80fd40adc2dce7ef
adriswara <chevellerz454@yahoo.com>
2024-04-06 16:18:27 +0700

    added template

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
-        template = loader.get_template('myFirst.html')
-        return HttpResponse(template.render())
-# Create your views here.
+        mymembers = Member.objects.all().values()
+        template = loader.get_template('all_members.html')
+        context = {
+                'mymembers': mymembers,
+        }
+        return HttpResponse(template.render(context, request))
